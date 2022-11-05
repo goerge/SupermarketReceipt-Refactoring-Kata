3253fb1cfc64757dde76a61626ee6e7a3744a4ad
Görge Albrecht <goerge@code-mentor.com>
2022-11-05 13:02:23 +0100

    extract discount strategies

--- java/src/main/java/dojo/supermarket/model/ShoppingCart.java
+++ java/src/main/java/dojo/supermarket/model/ShoppingCart.java
@@ -58,23 +58,36 @@ public class ShoppingCart {
 
         int quantityAsInt = (int) quantity;
         if (offer.offerType == SpecialOfferType.TWO_FOR_AMOUNT && quantityAsInt >= 2) {
-            int intDivision = quantityAsInt / 2;
-            double pricePerUnit = offer.argument * intDivision;
-            double theTotal = (quantityAsInt % 2) * unitPrice;
-            double total = pricePerUnit + theTotal;
-            double discountN = unitPrice * quantity - total;
-            return new Discount(product, "2 for " + offer.argument, -discountN);
+            return calculateTwoForAmmountDiscount(product, quantity, offer, unitPrice, quantityAsInt);
         }
-        int numberOfBulks = quantityAsInt / getBulkSize(offer);
         if (offer.offerType == SpecialOfferType.THREE_FOR_TWO && quantityAsInt > 2) {
-            double discountAmount = quantity * unitPrice - ((numberOfBulks * 2 * unitPrice) + quantityAsInt % 3 * unitPrice);
-            return new Discount(product, "3 for 2", -discountAmount);
+            return calculateThreeForTwoDiscount(product, quantity, offer, unitPrice, quantityAsInt);
         }
         if (offer.offerType == SpecialOfferType.FIVE_FOR_AMOUNT && quantityAsInt >= 5) {
-            double discountTotal = unitPrice * quantity - (offer.argument * numberOfBulks + quantityAsInt % 5 * unitPrice);
-            return new Discount(product, 5 + " for " + offer.argument, -discountTotal);
+            return calculateFiveForAmountDiscount(product, quantity, offer, unitPrice, quantityAsInt);
         }
         return null;
+    }
+
+    private static Discount calculateFiveForAmountDiscount(Product product, double quantity, Offer offer, double unitPrice, int quantityAsInt) {
+        int numberOfBulks = quantityAsInt / getBulkSize(offer);
+        double discountTotal = unitPrice * quantity - (offer.argument * numberOfBulks + quantityAsInt % 5 * unitPrice);
+        return new Discount(product, 5 + " for " + offer.argument, -discountTotal);
+    }
+
+    private static Discount calculateThreeForTwoDiscount(Product product, double quantity, Offer offer, double unitPrice, int quantityAsInt) {
+        int numberOfBulks = quantityAsInt / getBulkSize(offer);
+        double discountAmount = quantity * unitPrice - ((numberOfBulks * 2 * unitPrice) + quantityAsInt % 3 * unitPrice);
+        return new Discount(product, "3 for 2", -discountAmount);
+    }
+
+    private static Discount calculateTwoForAmmountDiscount(Product product, double quantity, Offer offer, double unitPrice, int quantityAsInt) {
+        int intDivision = quantityAsInt / 2;
+        double pricePerUnit = offer.argument * intDivision;
+        double theTotal = (quantityAsInt % 2) * unitPrice;
+        double total = pricePerUnit + theTotal;
+        double discountN = unitPrice * quantity - total;
+        return new Discount(product, "2 for " + offer.argument, -discountN);
     }
 
     private static int getBulkSize(Offer offer) {
